36b77b5273c6cb53632defc5a477a3df62a38873
Christian Dräger <christian.draeger1@gmail.com>
2019-03-14 18:23:00 +0100

    add logging for certain test that just starts

--- src/test/java/config/TestStatusLogger.kt
+++ src/test/java/config/TestStatusLogger.kt
@@ -1,18 +1,23 @@
 package config
 
+import org.junit.jupiter.api.extension.BeforeEachCallback
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.api.extension.TestWatcher
 import java.util.*
 
 
-class TestStatusLogger : TestWatcher {
+class TestStatusLogger : TestWatcher, BeforeEachCallback {
 
     private val ANSI_RESET = "\u001B[0m"
+
     private val ANSI_RED = "\u001B[31m"
     private val ANSI_GREEN = "\u001B[32m"
     private val ANSI_BLUE = "\u001B[34m"
+    private fun color(string: String, ansi: String) = "$ansi$string$ANSI_RESET"
 
-    fun color(string: String, ansi: String) = "$ansi$string$ANSI_RESET"
+    override fun beforeEach(context: ExtensionContext?) {
+        println(color(">>> test ${context!!.displayName} started", ANSI_BLUE))
+    }
 
     override fun testSuccessful(context: ExtensionContext?) {
         println(color("<<< test ${context!!.displayName} succeeded", ANSI_GREEN))
@@ -27,6 +32,6 @@ class TestStatusLogger : TestWatcher {
     }
 
     override fun testAborted(context: ExtensionContext?, cause: Throwable?) {
-        println(color("<<< test ${context!!.displayName} abortet", ANSI_BLUE))
+        println(color("<<< test ${context!!.displayName} aborted", ANSI_BLUE))
     }
 }
