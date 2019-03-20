48626e6e57420a3f9119d08faba6bab30c14743a
Christian Dräger <christian.draeger1@gmail.com>
2019-03-20 21:15:17 +0100

    try to fix travis ci build for osx and windows

--- .travis/prepare-win.sh
+++ .travis/prepare-win.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+choco install zip
+choco install unzip
+choco install ie11
 curl -s http://get.sdkman.io | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install java 8.0.202-zulu
