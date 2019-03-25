c71f4883b8c3ee0a66118216be3c691d81895c94
Christian Dräger <christian.draeger1@gmail.com>
2019-03-25 13:43:12 +0100

    adjust test status logger format a bit

--- src/test/java/config/TestStatusLogger.kt
+++ src/test/java/config/TestStatusLogger.kt
@@ -6,32 +6,33 @@ import org.junit.jupiter.api.extension.TestWatcher
 import java.util.*
 
 
-class TestStatusLogger : TestWatcher, BeforeEachCallback {
+class TestStatusLogger : BeforeEachCallback, TestWatcher {
 
-    private val ANSI_RESET = "\u001B[0m"
+    private val RESET = "\u001B[0m"
 
-    private val ANSI_RED = "\u001B[31m"
-    private val ANSI_GREEN = "\u001B[32m"
-    private val ANSI_BLUE = "\u001B[34m"
-    private fun color(string: String, ansi: String) = "$ansi$string$ANSI_RESET"
+    private val RED = "\u001B[31m"
+    private val GREEN = "\u001B[32m"
+    private val BLUE = "\u001B[34m"
+    private val WHITE = "\u001B[37m"
+    private fun color(string: String, ansi: String) = "$ansi$string$RESET"
 
     override fun beforeEach(context: ExtensionContext?) {
-        println(color(">>> test \"${context!!.displayName}\" - STARTED", ANSI_BLUE))
+        println(color("▶️ - test \"${context!!.displayName}\" has been started", BLUE))
     }
 
     override fun testSuccessful(context: ExtensionContext?) {
-        println(color("<<< test \"${context!!.displayName}\" - SUCCEEDED", ANSI_GREEN))
+        println(color("✅ - test \"${context!!.displayName}\" succeeded", GREEN))
     }
 
     override fun testFailed(context: ExtensionContext?, cause: Throwable?) {
-        println(color("<<< test \"${context!!.displayName}\" - FAILED", ANSI_RED))
+        println(color("⛔ - test \"${context!!.displayName}\" failed", RED))
     }
 
     override fun testDisabled(context: ExtensionContext?, reason: Optional<String>?) {
-        println(color("<<< test \"${context!!.displayName}\" - SKIPPED", ANSI_BLUE))
+        println(color("\uD83D\uDCA4 - test \"${context!!.displayName}\" has been skipped", WHITE))
     }
 
     override fun testAborted(context: ExtensionContext?, cause: Throwable?) {
-        println(color("<<< test ${context!!.displayName} - ABORTED", ANSI_BLUE))
+        println(color("\uD83D\uDD19 - test ${context!!.displayName} has been aborted", BLUE))
     }
 }
